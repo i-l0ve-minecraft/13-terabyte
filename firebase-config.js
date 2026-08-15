364969430497962ce7afe83f169054933f241e12
Dema <80444532+i-l0ve-minecraft@users.noreply.github.com>
2026-08-15 15:02:43 +0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,20 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyA92YjJblwdPwlQXat-aDf60rZCqldi6lU",
   authDomain: "terabytes-b0356.firebaseapp.com",
   projectId: "terabytes-b0356",
   storageBucket: "terabytes-b0356.firebasestorage.app",
   messagingSenderId: "30874792569",
-  appId: "1:30874792569:web:fecb1c18bfebedf1e2daa8"
+  appId: "1:30874792569:web:fecb1c18bfebedf1e2daa8",
+  measurementId: "G-FJ5D2RB5PZ"
 };
 
-// Инициализация Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Экспортируем ссылки на сервисы
-const db = firebase.firestore();
-const auth = firebase.auth();
-
-// Проверим, что всё инициализировалось
-console.log("Firebase initialized:", firebase.app().name);
-console.log("Firestore:", db ? "OK" : "FAILED");
-console.log("Auth:", auth ? "OK" : "FAILED");
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
